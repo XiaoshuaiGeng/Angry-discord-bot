23eec35ccb46815c06c50821cd739c8a2bae1f8b
XiaoshuaiGeng <xiaoshuaigeng@gmail.com>
2020-03-13 23:13:03 -0400

    update

--- index.js
+++ index.js
@@ -1,9 +1,68 @@
 const Discord = require('discord.js');
+const {prefix, token} = require('./config.json');
 const client = new Discord.Client();
+var https = require('https');
+var http = require('http');
 
-client.once('ready', () =>{
-    console.log('Ready')
 
-});
 
-client.login('Njg4MTQyMDY5MzMxNzg3Nzk3.XmwIgw.tuUsWCsaNarbh_7NHtucWrtVgFw');
+
+client.on('message', message => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+
+    const command = args.shift().toLowerCase();
+
+    if(command === '别骂了'){
+        message.channel.bulkDelete(10,true);
+        //delete last 10 messages
+
+    }
+
+    if(command === '瞅瞅'){
+        if(!message.mentions.users.size){
+            return message.reply('你瞅你妈呢？');
+        }
+
+        //message.channel.send(``)
+    }
+
+    if(command === 'kick'){
+        if(!message.mentions.users.size){
+            return message.reply('你踢人咋还不写名啊？');
+        }
+    }
+
+    if(command === '骂'){
+        if(!message.mentions.users.size){
+            return message.reply('骂人记得@他')
+        }
+        
+        
+        if(args.length > message.mentions.users.size){
+            var i;
+            for(i = 0; i < 5; i++){
+                message.channel.send(`${message.mentions.users.map(user => {return user})} ${args[args.length - 1]}`);
+    
+            }
+
+        }else{
+            var i;
+            for(i = 0; i < 5; i++){
+                message.channel.send(`${taggedUser} 给你妈一个一星差评`);
+    
+            }
+        }   
+    }
+
+    //message.channel.send(args[0]);
+
+    //@test if there is any arguments
+    // if(!args.length){
+    //     return message.channel.send(`No arguments`);
+    // }
+
+})
+
+client.login(token);
